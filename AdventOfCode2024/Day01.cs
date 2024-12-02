0a85235214a7a2e24d1c65a80f74637cd75709f0
Vignesh Mani <vigneshm21@protonmail.com>
2024-12-02 21:24:13 +0530

    2024 day01

--- AdventOfCode2024/Day01.cs
+++ AdventOfCode2024/Day01.cs
@@ -4,14 +4,57 @@ static class Day01
 {
     const string FILENAME = "resources/inputs/Day01.txt";
 
+    static (int[] Left, int[] Right) ParseFileAndReturnLists(string filename)
+    {
+        var lines = File.ReadAllLines(filename);
+        var left = new int[lines.Length];
+        var right = new int[lines.Length];
+        for (var i = 0; i < lines.Length; i++)
+        {
+            var nums = lines[i].Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            left[i] = int.Parse(nums[0]);
+            right[i] = int.Parse(nums[1]);
+        }
+        return (left, right);
+    }
+
+    static readonly (int[] Left, int[] Right) lists = ParseFileAndReturnLists(FILENAME);
+
     static int Part01()
     {
-        return -1;
+        var left = lists.Left.Order().ToArray();
+        var right = lists.Right.Order().ToArray();
+
+        var totalDistance = 0;
+        for (var i = 0; i < left.Length; i++)
+        {
+            totalDistance += Math.Abs(right[i] - left[i]);
+        }
+        return totalDistance;
     }
 
     static int Part02()
     {
-        return -1;
+        var rightCount = new Dictionary<int, int>();
+        foreach (var n in lists.Right)
+        {
+            if (rightCount.TryGetValue(n, out var val))
+            {
+                rightCount[n] = val + 1;
+                continue;
+            }
+            rightCount[n] = 1;
+        }
+
+        var similarityScore = 0;
+        foreach (var n in lists.Left)
+        {
+            if (rightCount.TryGetValue(n, out var val))
+            {
+                similarityScore += n * val;
+            }
+        }
+        return similarityScore;
     }
 
     public static void Main()
