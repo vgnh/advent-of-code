b3ef1c948ff2fe21236f2f04f3de25b9898354aa
Vignesh Mani <vigneshm21@protonmail.com>
2025-10-21 06:44:08 +0530

    init year 2021

---
+++ src/AdventOfCode2021/Day01.cs
@@ -0,0 +1,41 @@
+namespace AdventOfCode2021;
+
+static class Day01
+{
+    const string FILENAME = "resources/inputs/Day01.txt";
+
+    static readonly int[] Input = File.ReadAllLines(FILENAME).Select(int.Parse).ToArray();
+
+    static int Part01()
+    {
+        var count = 0;
+        for (var i = 0; i < Input.Length - 1; i++)
+        {
+            if (Input[i + 1] > Input[i])
+            {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    static int Part02()
+    {
+        var count = 0;
+        for (var i = 0; i < Input.Length - 3; i++)
+        {
+            if (Input[(i + 1)..(i + 4)].Sum() > Input[i..(i + 3)].Sum())
+            {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    internal static void Main()
+    {
+        Console.WriteLine("Advent of Code 2021, Day 01");
+        Console.WriteLine(Part01());
+        Console.WriteLine(Part02());
+    }
+}
---
+++ src/AdventOfCode2021/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Diagnostics;
+
+namespace AdventOfCode2021;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var stopwatch = Stopwatch.StartNew();
+
+        var actions = new List<Action>
+        {
+            Day01.Main,
+            // Day02.Main,
+            // Day03.Main,
+            // Day04.Main
+        };
+        actions.ForEach(act => act.Invoke());
+
+        stopwatch.Stop();
+        Console.WriteLine($"\nTime elapsed: {stopwatch.ElapsedMilliseconds / 1000f:F6}s");
+    }
+}
